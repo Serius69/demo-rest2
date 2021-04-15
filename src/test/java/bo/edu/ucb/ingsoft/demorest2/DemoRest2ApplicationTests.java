7864faf6a1997bbeb9277390b42a59ee155b32c2
Sergio Troche <sergio.troche@ucb.edu.bo>
2021-04-15 15:56:23 -0400

    Agregando repositorio

---
+++ src/test/java/bo/edu/ucb/ingsoft/demorest2/DemoRest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.ingsoft.demorest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
